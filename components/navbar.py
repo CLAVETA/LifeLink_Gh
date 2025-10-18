a846c9751c18c18fabe60fc2ea78f86fa6ccb75d
Aqualinqs Digital <aqualinqs22@gmail.com>
2025-10-18 15:21:05 +0000

    Update Education link on nav bar

--- components/navbar.py
+++ components/navbar.py
@@ -7,10 +7,10 @@ def show_navbar():
                 ui.image("/assets/logo.png").classes("w-12 h-12")
                 ui.label("LifeLink GH").classes("text-xl font-bold text-gray-700")
             with ui.row().classes("gap-6 mt-3 md:mt-0"):
-                ui.link("About","/about").classes("no-underline text-gray-700 hover:text-red transition")
-                ui.link("Education","/education").classes("no-underline text-gray-700 hover:text-red transition")
-                ui.link("Contact").classes("no-underline text-gray-700 hover:text-red transition")
-                ui.link("FAQs").classes("no-underline text-gray-700 hover:text-red transition")
+                ui.link("About","/about").classes("no-underline text-gray-700 hover:text-red-500 transition")
+                ui.link("Education","/user_education").classes("no-underline text-gray-700 hover:text-red-500 transition")
+                ui.link("Contact").classes("no-underline text-gray-700 hover:text-red-500 transition")
+                ui.link("FAQs").classes("no-underline text-gray-700 hover:text-red-500 transition")
             with ui.row().classes("gap-3 mt-3 md:mt-0"):
                 ui.button("Register as Donor", on_click=lambda: ui.navigate.to("/donor_registration")).props("no-caps flat dense").classes("bg-red-600 text-white hover:bg-red-500 rounded-md px-4")
                 ui.button("Hospital Login").props("no-caps flat dense").classes("bg-pink-200 text-red rounded-md px-4")
